097ef9e64de8c7c4d23690dfb24e7f92e754c597
aleo74 <6uWMdr8iF6YH4g>
2023-12-04 10:55:28 +0100

    feat: add edit stock testing

--- tests/test_stocks.py
+++ tests/test_stocks.py
@@ -36,6 +36,30 @@ class StocksControllerTestCase(unittest.TestCase):
         headers = {'Authorization': 'Bearer ' + token}
         response = self.client.get('/get_stocks/test', headers=headers)
 
+    def test_edit_stock(self):
+        token = self.get_jwt_token()
+        headers = {'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json'}
+
+        data = json.dumps({'name': 'testUpdate', 'desc': 'description de test', 'qty': 1})
+        self.client.post('/save_stock', data=data, headers=headers)
+
+        response = self.client.get('/get_one_stock/testUpdate', headers=headers)
+        stock_data = json.loads(response.data)
+
+        updated_data = {'qty': 2}
+        response = self.client.put(f'/edit_stock/{stock_data["_id"]}', data=json.dumps(updated_data), headers=headers)
+
+        self.assertEqual(response.status_code, 200)
+        expected_data = {"message": "Stock mis à jour avec succès"}
+        response_data = json.loads(response.data)
+        self.assertEqual(expected_data, response_data)
+
+        response = self.client.get(f'/get_one_stock/test_updated', headers=headers)
+        updated_stock_data = json.loads(response.data)
+        self.assertEqual(updated_stock_data['name'], 'test_updated')
+        self.assertEqual(updated_stock_data['desc'], 'description mise à jour')
+        self.assertEqual(updated_stock_data['qty'], 2)
+
 
 if __name__ == '__main__':
     unittest.main()
