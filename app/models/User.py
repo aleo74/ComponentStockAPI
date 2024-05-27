90dc71d3c3c669e8552bd083b1ee2032df74e731
aleo74 <6uWMdr8iF6YH4g>
2024-05-27 13:07:44 +0200

    fix: delete useless print and comment

--- app/models/User.py
+++ app/models/User.py
@@ -6,13 +6,11 @@ class User:
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
-        self.password = generate_password_hash(password)  # Hachage du mot de passe
+        self.password = generate_password_hash(password)
         self.created_at = datetime.utcnow()
 
     def to_dict(self):
         return self.__dict__
 
     def check_password(self, password):
-        print(self.password)
-        print(password)
         return check_password_hash(self.password, password)
